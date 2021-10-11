ed5633c531c1b63ef494ee351b0b7eefaab1a752
David Flores <noshi_1@hotmail.com>
2021-10-11 19:06:00 -0300

    Se incorporo un archivo de servidor para servir publicamente los archivos en produccion

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     entry: './src/index.js',
     output: {
         path: `${__dirname}/dist`,
-        filename: 'index.js'
+        filename: 'src/index.js'
     },
     plugins: [
         new HTMLWebpackPlugin({
