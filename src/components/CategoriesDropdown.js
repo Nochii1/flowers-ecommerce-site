bd24cd3bd646c134691654addaaebe3b059607df
David Flores <noshi_1@hotmail.com>
2021-10-11 22:09:23 -0300

    Fix: IOS categories dropdown hover

--- src/components/CategoriesDropdown.js
+++ src/components/CategoriesDropdown.js
@@ -12,7 +12,7 @@ const CategoriesDropdown = {
         const {category} = getRequestUrl()
         return ` 
             <div id="dropdown" class="dropdown" >
-                <label class="dropbtn flex flex-row items-center space-x-1 text-gray-800 hover:text-gray-400 cursor-pointer">
+                <label onClick="return true" class="dropbtn flex flex-row items-center space-x-1 text-gray-800 hover:text-gray-400 cursor-pointer">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
                     </svg>
